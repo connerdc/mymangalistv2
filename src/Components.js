e3b2fa5a74f13a8ec5845fa998ee8f29ead2d51d
Conner Christopherson <cdcqq7@gmail.com>
2019-12-14 01:56:30 -0600

    nevermind, got it to work

--- src/Components.js
+++ src/Components.js
@@ -3,16 +3,13 @@ const axios = require('axios').default;
 
 function Page() {
     //const [getUrl, setGetUrl] = useState('https://www.reddit.com/hot.json');
-    const [getUrl, setGetUrl] = useState('https://www.reddit.com/r/manga/search?q=boku+no+hero+academia&limit=10.json');
+    const [getUrl, setGetUrl] = useState('https://www.reddit.com/r/manga/search.json?q=my+hero+academia&restrict_sr=true&limit=10');
     const [apiResponse, setApiResponse] = useState({});
 
     useEffect(() => {
         axios({
             method: 'get',
-            url: getUrl,
-            data: {
-                'Content-Type':  'application/json' 
-            }
+            url: getUrl
         })
         .then(function(response) {
             setApiResponse(response);
